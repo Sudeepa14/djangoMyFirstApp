b335efd5bb6404d5ce61c5b6592223adfa8e2954
sudeepa14 <sudeepa.14@cse.mrt.ac.lk>
2017-09-27 12:05:06 +0100

    First Commit

---
+++ myfirstapp/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from django.conf.urls import include
+from . import views
+
+urlpatterns = [
+    url(r'^$',views.index, name='index')
+    ]
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$',views.index, name='index')
+    ]
